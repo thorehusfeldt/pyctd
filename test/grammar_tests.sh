240e2d69e37bacf2e21c40e4a4491bf349ba7a14
mzuenni <michi.zuendorf@gmail.com>
2026-03-07 04:07:47 +0100

    add more tests

---
+++ test/grammar_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s nullglob
+cd "$(dirname "$0")"
+
+check() {
+    local cmd=("$@")
+    if "${cmd[@]}" "$prog" < "$data" > /dev/null 2>&1; then
+        echo "0"
+    else
+        echo "$?"
+    fi
+}
+
+for prog in grammar/test_*_prog; do
+    base="${prog%_prog}"
+    echo "${base}"
+    data="${base}"_data
+
+    expected=$(check ../third_party/checktestdata)
+    got=$(check python3 ../checktestdata/)
+
+    #echo "Got: ${got}, expected: ${expected}"
+    if (( (expected == 0) != (got == 0) )); then
+        echo "Got: ${got}, expected: ${expected}"
+        exit 1
+    fi
+done
